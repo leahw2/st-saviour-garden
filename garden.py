6aecb6d2717e8ebb98a8dcf605f02dae78390f3a
mwilliams <mwilliams28@stsaviour.org>
2026-03-13 21:54:12 -0400

    food

--- garden.py
+++ garden.py
@@ -1,6 +1,45 @@
 
 
 if __name__ == '__main__':
+    for i in range(11):
+        for j in range(11):
+             print('🌷', end='')
+        print ('')
 
-    # print without a line break using this function
-    print('🌷', end='')
+    print('horizontal')
+    for i in range(11):
+        for j in range(11):
+            if i % 2 == 0:
+                print('🌷', end='') 
+            else:
+                print('🌼', end='')  
+        print('')
+
+    print('vertical')
+    for i in range(11):
+        for j in range(11):
+            if j % 2 == 0: 
+                 print('🌷', end='') 
+            else:
+                print('🌼', end='')  
+        print('')
+
+    print('diagnol')
+    for i in range(11):
+        for j in range(11):
+            if (i+j) % 2 == 0:
+                 print('🌷', end='') 
+            else:
+                print('🌼', end='')  
+        print('')
+
+    print('plaid')
+    for i in range(11):
+        for j in range(11):
+            if i % 2 == 0 and j % 2 == 0:
+                 print('🌷', end='') 
+            else:
+                print('🌼', end='')  
+        print('')
+
+            
